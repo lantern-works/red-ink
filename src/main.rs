83d959d2d93be1f01fd2ddbcbf2274968e00475d
Jonathan Dahan <hi@jonathan.is>
2019-02-08 15:13:53 -0500

    Revert "remove unused LUT"
    This reverts commit d89e0398e28218ecc0b8aa3aee8f291daa5c2584.

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,27 @@ const ROWS: u16 = 212;
 const COLS: u8 = 104;
 
 #[rustfmt::skip]
+const LUT_YELLOW: [u8; 70] = [
+    // Phase 0     Phase 1     Phase 2     Phase 3     Phase 4     Phase 5     Phase 6
+    // A B C D     A B C D     A B C D     A B C D     A B C D     A B C D     A B C D
+    0b11111010, 0b10010100, 0b10001100, 0b11000000, 0b11010000,  0b00000000, 0b00000000,  // LUT0 - Black
+    0b11111010, 0b10010100, 0b00101100, 0b10000000, 0b11100000,  0b00000000, 0b00000000,  // LUTT1 - White
+    0b11111010, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  0b00000000, 0b00000000,  // IGNORE
+    0b11111010, 0b10010100, 0b11111000, 0b10000000, 0b01010000,  0b00000000, 0b11001100,  // LUT3 - Yellow (or Red)
+    0b10111111, 0b01011000, 0b11111100, 0b10000000, 0b11010000,  0b00000000, 0b00010001,  // LUT4 - VCOM
+
+    // Duration            | Repeat
+    // A   B     C     D   |
+    64,   16,   64,   16,   8,
+    8,    16,   4,    4,    16,
+    8,    8,    3,    8,    32,
+    8,    4,    0,    0,    16,
+    16,   8,    8,    0,    32,
+    0,    0,    0,    0,    0,
+    0,    0,    0,    0,    0,
+];
+
+#[rustfmt::skip]
 const LUT_FAST_YELLOW: [u8; 70] = [
     // Phase 0     Phase 1     Phase 2     Phase 3     Phase 4     Phase 5     Phase 6
     // A B C D     A B C D     A B C D     A B C D     A B C D     A B C D     A B C D
@@ -147,7 +168,7 @@ fn main() -> Result<(), std::io::Error> {
       Coord::new(2, 4 + (line * (14 + 4)))
     ).collect::<Vec<Coord>>();
 
-    let mut render_start;
+    let mut render_start = Instant::now();
     let mut last_render_time = Duration::new(0, 1);
 
     loop {
